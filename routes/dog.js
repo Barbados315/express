d79613b94f1dfe674f380d17e3f4d06b4968e29d
Barbados315 <2201100238vx@cyber-u.ac.jp>
2025-01-30 00:23:26 +0900

    Create dog.js

---
+++ routes/dog.js
@@ -0,0 +1,8 @@
+async function displayRandomDog(){       
+        const url = "https://dog.ceo/api/breeds/image/random";
+        const response = await fetch(url)
+        data = await response.json();       
+        const object = document.getElementById("randomDog");
+        object.innerHTML = `<img src="${data.message}">`;
+}
+displayRandomDog();
